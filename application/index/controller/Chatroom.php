4a715992e30c8239058248e5256c88febf6381a9
WeidiDeng <weidi_deng@icloud.com>
2019-04-08 08:16:15 +0800

    添加用户加入离开聊天室逻辑

--- application/index/controller/Chatroom.php
+++ application/index/controller/Chatroom.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\index\controller;
 
+use app\index\model\Blacklist;
+use app\index\model\ChatHistory;
 use think\Controller;
 use think\facade\Session;
 
@@ -14,12 +16,16 @@ class Chatroom extends Controller
         if (!Session::get("logged_name")) {
             $this->redirect(url("index/index"));
         }
+        // 更新cookie和session有效时常
         $sessid = cookie('PHPSESSID');
         cookie('PHPSESSID', $sessid, 300);
         $res = OnlineUser::get(['sess_id'=>$sessid]);
+        $chatroom = Session::get("current_chat");
+        $res->chatroom_hash = $chatroom;
         $res->save();
-        if (Session::get("current_chat")) {
-            $ignoredAction = ["room", "to_index", "logout", "join_room"];
+
+        if ($chatroom) {
+            $ignoredAction = ["room", "to_index", "logout", "join_room", "post_message", "get_message"];
             if (!in_array(request()->action(), $ignoredAction)) {
                 $this->redirect("chatroom/room", ["hash"=>Session::get("current_chat")]);
             }
@@ -30,6 +36,16 @@ class Chatroom extends Controller
         return $this->fetch("index");
     }
     public function logout() {
+        $chat = Session::get("current_chat");
+        if ($chat) {
+            ChatHistory::create([
+                "chatroom_id"=>ChatroomModel::get(["hash"=>$chat])->id,
+                "name"=>Session::get("logged_name"),
+                "type"=>0,
+                "content"=>Session::get("logged_name")."已离开聊天室"
+            ]);
+            ChatroomModel::get(['hash'=>$chat])->setDec("count");
+        }
         Session::clear();
         $this->success("退出登陆成功",url("index/index/index"));
     }
@@ -69,21 +85,97 @@ class Chatroom extends Controller
     // 加入聊天室
     public function join_room($hash)
     {
-        if (Session::get("current_chat")) {
-            Session::delete("current_chat");
+        $res = ChatroomModel::get(['hash'=>$hash]);
+        if (empty($res)) {
+            $this->error("聊天室不存在");
+        }
+        if ($res->count == $res->max_count) {
+            $this->error("聊天室已满");
+        }
+        $old_hash = Session::get("current_chat");
+        if ($old_hash && $old_hash!=$hash) {
+            ChatHistory::create([
+                "chatroom_id"=>ChatroomModel::get(["hash"=>$old_hash])->id,
+                "name"=>Session::get("logged_name"),
+                "type"=>0,
+                "content"=>Session::get("logged_name")."已离开聊天室"
+            ]);
+            ChatroomModel::get(['hash'=>$old_hash])->setDec("count");
         }
         Session::set("current_chat", $hash);
+        ChatHistory::create([
+            "chatroom_id"=>ChatroomModel::get(["hash"=>$hash])->id,
+            "name"=>Session::get("logged_name"),
+            "type"=>0,
+            "content"=>Session::get("logged_name")."已加入聊天室"
+        ]);
+        $res->setInc("count");
         $this->redirect("chatroom/room", ["hash"=>$hash]);
     }
     // 退出聊天室
     public function to_index()
     {
+        $old_hash = Session::get("current_chat");
+        if ($old_hash) {
+            ChatHistory::create([
+                    "chatroom_id"=>ChatroomModel::get(["hash"=>$old_hash])->id,
+                    "name"=>Session::get("logged_name"),
+                    "type"=>0,
+                    "content"=>Session::get("logged_name")."已离开聊天室"
+            ]);
+            ChatroomModel::get(['hash'=>$old_hash])->setDec("count");
+        }
         Session::delete("current_chat");
         $this->redirect("chatroom/index");
     }
+    // 列出聊天室
     public function list_chatroom()
     {
-        $res = ChatroomModel::all();
+        $res = ChatroomModel::all()->where("count",">", 0);
         return json($res);
+    }
+    // 发送消息
+    public function post_message()
+    {
+        $message = input("post.message");
+        if (empty($message)) {
+            return json(array("code"=>-1,"msg"=>"必须发送消息"));
+        }
+        $chatroom = Session::get("current_chat");
+        if (!$chatroom) {
+            return json(array("code"=>-1,"msg"=>"目前没有加入聊天室"));
+        }
+
+        // 关键字检测
+        $black_list = Blacklist::all()->column("content");
+        foreach ($black_list as $black_word) {
+            if (strpos($message, $black_word)!==false) {
+                return json(array("code"=>-1,"msg"=>"敏感词"));
+            }
+        }
+
+        $chatroom_id = ChatroomModel::get(["hash"=>$chatroom])->id;
+        $logged_name = Session::get("logged_name");
+        ChatHistory::create([
+            "chatroom_id"=>$chatroom_id,
+            "name"=>$logged_name,
+            "content"=>$message,
+        ]);
+        return json(array("code"=>0,"msg"=>"ok"));
+    }
+    // 获取消息
+    public function get_message()
+    {
+        $chatroom = Session::get("current_chat");
+        if (!$chatroom) {
+            return json(array("code"=>-1,"msg"=>"目前没有加入聊天室"));
+        }
+        $cnt = input("get.cnt/d");
+        if (empty($cnt)) {
+            $cnt = 0;
+        }
+        $chatroom_id = ChatroomModel::get(["hash"=>$chatroom])->id;
+        $history = ChatHistory::all(["chatroom_id"=>$chatroom_id])->where("id", ">", $cnt);
+        return json(array("code"=>0,"msg"=>$history));
     }
 }
